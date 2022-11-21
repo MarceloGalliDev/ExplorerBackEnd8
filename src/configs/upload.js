79623f50e8a5f0da6bd81078138319650773b90e
Marcelo Galli <marcelolemesgalli@hotmail.com>
2022-11-21 20:34:15 -0300

    update
    update

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 const crypto = require("crypto");
 
 const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");//__dirname, "..", quando uso ".." é para o uso de localização do caminho e o tmp é a pasta que eu busco
-const UPLOADS_FOLDER = path.resolve(__dirname, "uploads");
+const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, "uploads");
 
 const MULTER = {
     storage: multer.diskStorage({
--- src/providers/DiskStorage.js
+++ src/providers/DiskStorage.js
@@ -7,12 +7,37 @@ class DiskStorage {
         await fs.promises.rename(
             path.resolve(uploadConfig.TMP_FOLDER, file),
             path.resolve(uploadConfig.UPLOADS_FOLDER, file)
-        )//rename usamos para mover o arquivo de lugar
+        )//rename usamos para mover o arquivo de lugar, mas da para usar para renomear um arquivo
 
         return file;
     }
 
     async deleteFile(file) {
-        const filePa 
+        const filePath = path.resolve(uploadCOnfig.UPLOADS_FOLDER, file);
+        try {
+            await fs.promises.stat(filePath);
+        } catch {
+            return;
+        }
+
+        await fs.promises.unlink(filePath);
     }
 }
+
+module.exports = DiskStorage;
+
+//usamos async e await com fs.promises, para que seja executado depois de realizando a execução das funções
+//path.resolve, resolve uma seuqência de segmentos de caminho para um caminho absoluto, de acordo com o sistema operacional
+/*
+      const filePath = path.resolve(uploadCOnfig.UPLOADS_FOLDER, file);
+        try {
+            await fs.promises.stat(filePath);
+        } catch {
+            return;
+        }
+
+        await fs.promises.unlink(filePath);
+
+aqui aqui usamos o modulo fs para verificar o estado do arquivo, se esta aberto, corrompido entre outros.
+unlink é para remover o arquivo
+*/
