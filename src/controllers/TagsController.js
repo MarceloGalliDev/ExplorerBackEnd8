1c4c9537d13a62f7aa2dc9a88e59e1fde5c6d9c3
Marcelo Galli <marcelolemesgalli@hotmail.com>
2022-11-27 17:43:00 -0300

    update
    update

--- src/controllers/TagsController.js
+++ src/controllers/TagsController.js
@@ -6,6 +6,7 @@ class TagsController {
 
         const tags = await knex("tags")
         .where({ user_id }) //como o user_id tem o mesmo nome da variavel de request.params eu nao preciso passar user_id: user_is
+        .groupBy("name") //estamos agrupando por nome as tags e remove nomes duplicados
         
         return response.json(tags)
     }
