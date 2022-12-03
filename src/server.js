5977c5f7d86f5fee224396f24d649adcf5692032
Marcelo Galli <marcelolemesgalli@hotmail.com>
2022-12-03 17:12:10 -0300

    update
    update

--- src/server.js
+++ src/server.js
@@ -36,5 +36,5 @@ app.use((error, request, response, next) => {
     });
 });
 
-const PORT = 3333;
+const PORT = process.env.SERVER_PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`))
